d88653cf9cb3367c56bbb8fe89e90481c7d4795e
vinod_wijesooriya <vinodw@opusxenta.com>
2025-04-13 13:42:45 +0530

    intial commit

---
+++ demo-for-vue3/src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1>My App</h1>
+  <MyButton label="Click Me" />
+</template>
+
+<script lang="ts" setup>
+import MyButton from './components/MyButton.vue'
+</script>
---
+++ demo-for-vue3/src/components/MyButton.vue
@@ -0,0 +1,14 @@
+<template>
+    <button @click="handleClick">{{ label }}</button>
+  </template>
+  
+  <script lang="ts" setup>
+  defineProps<{
+    label: string
+  }>()
+  
+  function handleClick() {
+    alert('Button clicked!')
+  }
+  </script>
+  
